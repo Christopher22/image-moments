eb7982ced0d8e272ed164af02c2a150fb96f0d58
Christopher22 <christopher@gundler.de>
2021-11-30 16:29:40 +0000

    Deploying to documentation from @ Christopher22/image-moments@0b1d62622f8d58d26a3b640a2d67cbcfc90a1cb9 🚀

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["image_moments"] = [{"text":"impl&lt;const O:&nbsp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.56.1/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"image_moments/struct.Order.html\" title=\"struct image_moments::Order\">Order</a>&lt;O&gt;","synthetic":false,"types":["image_moments::order::Order"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["image_moments"] = {"name":"","dirs":[{"name":"implementation","files":["accumulator.rs","central_moments.rs","mod.rs","normalized_central_moments.rs","storage.rs"]}],"files":["central.rs","index.rs","lib.rs","moments.rs","normalized_central.rs","order.rs","primitives.rs","spatial.rs"]};
+createSourceSidebar();
